19b85042cf2c00a7b4a962a414c2a8eb174687e1
Ela Malouche <elamalouche6@gmail.com>
2026-03-09 13:38:28 +0100

    fix: remove duplicate title

--- frontend/src/pages/NouveauTicket.jsx
+++ frontend/src/pages/NouveauTicket.jsx
@@ -1,12 +1,7 @@
 import TicketForm from '../components/TicketForm';
 
 function NouveauTicket() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Créer un nouveau ticket</h1>
-      <TicketForm />
-    </div>
-  );
+  return <TicketForm />;
 }
 
 export default NouveauTicket;
